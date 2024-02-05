63c13fe9f63b12cad7abb7acd38801b79fc6f637
DDT13163 <darklordzeratio13@gmail.com>
2024-02-05 10:17:54 +0700

    Update Learning 05/02/2024

---
+++ My_First_Web_App/src/test/java/com/example/my_first_web_app/MyFirstWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_first_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
