e22eb14d2a42c9c2343a4288380e8d0dc3befe69
DDT13163 <darklordzeratio13@gmail.com>
2024-03-21 17:24:43 +0700

    update learning 21/03/2024

--- Full-Stack/front-end/todo-app/src/components/todo/WelcomeComponent.jsx
+++ Full-Stack/front-end/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,8 +1,24 @@
+import axios from 'axios'
 import { useParams, Link } from 'react-router-dom'
 
 export default function WelcomeComponent() {
 
     const { username } = useParams()
+
+    function callHelloWorldRestApi() {
+        axios.get('http://localhost:8080/hello-world')
+        .then( (response) => successfullyResponse(response) )
+        .catch( (error) => errorResponse(error) )
+        .finally( () => console.log('cleanup'))
+    }
+
+    function successfullyResponse(response) {
+        console.log(response)
+    }
+
+    function errorResponse(error) {
+        console.log(error)
+    }
 
     return (
         <div className="WelcomeComponent">
@@ -10,7 +26,11 @@ export default function WelcomeComponent() {
             <div>
                 Manage your todos - <Link to="/todos">Go here</Link>
             </div>
-            Welcome Component
+            <div>
+                <button className='btn btn-success m-5' onClick={callHelloWorldRestApi}>
+                    Call Hello World
+                </button>
+            </div>
         </div>
     )
 }
